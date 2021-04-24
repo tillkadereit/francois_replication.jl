96996b099df5879d50083427986b2d7415a73f86
Till Kadereit <till.kadereit@sciencespo.fr>
2021-04-24 21:49:52 +0200

    start splitlikelihood

--- src/francois_replication.jl
+++ src/francois_replication.jl
@@ -10,9 +10,7 @@ Pkg.add("XLSX")
 
 using XLSX
 
-xf = XLSX.readxlsx("C:\\Users\\Till\\Documents\\Uni\\Paris\\4thSemester\\Numerical_Methods\\Replication_Project\\Replication_kit_francois\\ECMA_Replication_FrancoisRainerTrebbi\\Matlab_files\\ethnicgroup_sorted2.XLSX")
-XLSX.sheetnames(xf)
-sh = xf["ethnicgroup_data"]
+xf  = XLSX.readdata("ethnicgroup_sorted2.XLSX", "ethnicgroup_data", "A2:Y11750")
 
 
 
@@ -68,25 +66,25 @@ regimes[1,1] = NaN  # used to check that leader is found
 regimes[1,3] = 1     # first transition associated with first observation 
 
 
-
-for i in [1:size(popshare,1)]
-    if (regimechange .== 0 && leadertrans(i) .== 1 && i%numeth .== 1)
+for i=(1:size(popshare,1))       
+    # look for transition only every numeth rows
+    if (regimechange == 0 && leadertrans[i] == 1 && mod(i, numeth) .== 1)
         regimechange = 1
         regimes[idx,2] = (i-startofregime)/numeth
         startofregime = i
         idx = idx + 1
-        regimes[idx,1] = NaN                   # used to check that leader is found
-        regimes[idx,3] = i                     #observation at which transition occurred
+        regimes[idx,1] = NaN  # used to check that leader is found
+        regimes[idx,3] = i    # observation at which transition occurred
     end
     if regimechange .== 1
         # looking for new leader
         if leader[i] == 1
             # found leader of new regime
-            regimes[idx,1] = i%numeth
+            regimes[idx,1] = mod(i, numeth)
             if regimes[idx,1] .== 0
-                regimes[idx,1] .= numeth
+                regimes[idx,1] = numeth
             end
-            regimechange .= 0
+            regimechange = 0
         end
     end
     
@@ -95,8 +93,11 @@ end
 
 
 # length of last regime
+
 # regimechange should be zero as we should have found the last start of
 # regime & then the leader of that regime
+
+regimechange = 0
 
 if regimechange .== 0 
     regimes[idx,2] = (size(popshare,1)+1-startofregime)/numeth
@@ -106,8 +107,113 @@ end
 
 
 # check that leaders were always found
-if (sum(isnan(regimes[:,dims=1])))
+if (sum(isnan(regimes[:,1])))
     error("A leader was not found for at least one regime.")
 end
 
-sum(isnan(regimes; dims=1))
+
+
+
+# the parameter vector to be estimated is (first to last)
+
+# Fprime - leadership payoff; transformed
+# gamma_t - probability of successful coup [transformed]
+# xi - parameter governing [beta] error distribution
+# rprime = delta*epsilon*(1-r)/r/(1-delta) where delta is the
+# discount factor; epsilon is the probability of regime change; & r is()
+# the factor by which patronage is destroyed when a revolution occurs
+
+
+
+
+# fix alpha & epsilon at estimates from pooled estimation
+alpha = 11.5222
+epsilon = 0.115/(1-0.115) # transformed from estimate obtained
+
+
+
+
+myll = (paramvec) ->splitlikelihood[paramvec,popshare,govshare,P,regimes,includeobs,numeth,lambda,0,0,[alpha epsilon],0]
+
+
+
+
+
+#create bounds for the initial population (not binding during optimization)
+
+
+initrange = [0.25 0 40 0; 2 10 250 1]
+
+#set options for ga algorithm
+options = gaoptimset["PopulationSize",1000,"Generations",200,"TolFun",1E-10,"PopInitRange",initrange]
+
+
+#if an estimate from pooledestimation exists, place in initial population
+
+if (exist("countryparam","var")) ~= 0
+    disp("Using estimate from pooledestimation in initial population.")
+    options = gaoptimset[options,"InitialPopulation",countryparam]
+end
+
+
+#estimation
+[paramestga fvalga exitflag] = ga[myll,4,options]
+
+
+#local optimization (options and estimation)
+fminopts = optimset("Display","off","TolX",1e-6,"TolFun",1e-10, "MaxIter", 10000,"MaxFunEvals",10000)
+[paramest,fval,fminflag] = fminsearch(myll,paramestga,fminopts)
+
+if fminflag~=1
+    disp("WARNING: fmin failed to converge.")
+end
+
+
+
+# report likelihood & check that insider constraint is not violated
+
+[ll,constflag] = splitlikelihood[paramest,popshare,govshare,P,regimes,includeobs,numeth,lambda,0,0,[alpha epsilon],1]
+
+if constflag .== 1
+    error ("Insider constraint is violated at estimated parameters.")
+end
+
+
+
+# calculate standard errors using the outer product of gradients
+# likelihood function returns the vector of likelihoods rather than total
+
+var = OPG["splitlikelihood",paramest,popshare,govshare,P,regimes,includeobs,numeth,lambda,1,0,[alpha epsilon],0]
+
+
+
+seorig = sqrt(diag(inv(var))); # untransformed std errors
+
+
+# transform variables
+
+gamma = abs(paramest[2])/(1+abs(paramest[2]))
+F = paramest[1]*(1-gamma)/gamma()
+
+
+J = zeros(size(paramest,2),size(paramest,2))
+J[1,1] = (1-gamma)/gamma; # derivative of F wrt F'
+J[1,2] = -paramest[1]/gamma^2*transformder[paramest[2]]; # derivative of F wrt gamma()
+J[2,2] = transformder[paramest[2]]; # derivative of transformation for gamma()
+J[3,3] = 1; # no transformation for xi
+J[4,4] = 1; # no transformation for rprime
+
+
+
+# transform & report estimates
+
+disp("The estimated parameters are: F, gamma, xi, rprime, Fprime")
+
+
+
+[F gamma paramest[3] paramest[4] paramest[1]]
+
+disp("with standard errors:")
+se = sqrt(diag(J*inv(var)*J')); #standard errors
+
+[se[1] se[2] se[3] se[4] seorig[1]]
---
+++ src/francois_replication2.jl
@@ -0,0 +1,5 @@
+
+
+function splitlikelihood(paramvec,popshare,govshare,P,regimes,includeobs,numeth,lambda,vectorize,estregimes,ae,final)
+return [ll,constflag,sharedata,members]
+end
